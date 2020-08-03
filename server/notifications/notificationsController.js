7bce316fec84a607ca5829b944749c7a3675c86b
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-08-03 14:15:30 -0700

    Logic to verify Amazon SNS subscription

--- server/notifications/notificationsController.js
+++ server/notifications/notificationsController.js
@@ -1,6 +1,44 @@
+const express = require('express')
 const bodyParser = require('body-parser');
+const axios = require('axios')
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 module.exports.removeBouncedEmail = async (req, res) => {
-  console.log('req: ', req.body.SubscribeURL);
-  console.log('req: ', req);
-};
+
+  (req, res) => {
+    let body = ''
+
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    })
+
+    req.on('end', () => {
+      let payload = JSON.parse(body)
+
+      if (payload.Type === 'SubscriptionConfirmation') {
+        const promise = new Promise((resolve, reject) => {
+          const url = payload.SubscribeURL
+
+          axios(url, (error, response) => {
+            if (!error && response.statusCode == 200) {
+              console.log('Yess! We have accepted the confirmation from AWS')
+              return resolve()
+            } else {
+              return reject()
+            }
+          })
+        })
+
+        promise.then(() => {
+          res.end("ok")
+        })
+      }
+    })
+  
+    console.log('req: ', req.body.SubscribeURL);
+    console.log('req: ', req);
+
+  };
+}
