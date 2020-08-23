d976d19e0522d50deedcff38a7b99e39fc3f2db9
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-08-23 12:27:29 -0700

    wss url fix

--- client/src/components/Uppy.vue
+++ client/src/components/Uppy.vue
@@ -77,7 +77,7 @@ export default {
       uppy.getPlugin('Dashboard').openModal();
     };
 
-    const rws = new ReconnectingWebSocket(process.env.VUE_APP_SERVER.replace('https', 'wss'));
+    const rws = new ReconnectingWebSocket(process.env.VUE_APP_WSS);
 
     rws.onclose = () => {
       console.log('WebSocket is closed.');
