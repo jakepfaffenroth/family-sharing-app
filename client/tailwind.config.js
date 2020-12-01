f34a55b446e08c16449b18f2cc11b7e2332e9931
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-12-01 13:58:29 -0800

    HOTFIX: fixed client tailwind purging

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: ['./components/**/*.{vue,js}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
