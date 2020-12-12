1e8da44f80034c62ef8e31765c161a2d064f3036
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-12-11 21:43:31 -0800

    Made SSR menu responsive Fixed tailwind Code cleanup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     extend: {
       spacing: { '46': '11rem', '90vw': '90vw', '70vw': '70vw' },
--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,9 +1,22 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./public/**/*.html', './**/*.hbs'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: { 46: '11rem', '90vw': '90vw', '70vw': '70vw' },
+      maxHeight: { '9/10': '90%' },
+      maxWidth: { '50vw': '50vw', '90vw': '90vw' },
+      colors: {
+        teal: colors.teal,
+        orange: colors.orange,
+        gray: {
+          450: '#84849a',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/custom-forms')],
-  experimental: 'all',
+  // experimental: 'all',
 };
