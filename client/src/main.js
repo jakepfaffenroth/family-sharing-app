73d1caa2e1967359f6e39e59f69704f4b583aed7
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-12-26 13:39:52 -0800

    Enabled vue devtools in production

--- client/src/main.js
+++ client/src/main.js
@@ -7,6 +7,8 @@ import store from './store';
 import keepInViewPortDirective from './utils/keepInViewportDirective.js';
 
 const app = createApp(App).use(store);
+app.config.devtools = true;
+window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app.constructor;
 
 app.directive('keepInViewport', keepInViewPortDirective);
 
