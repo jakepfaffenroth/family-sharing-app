59f0e2e8937e6777a8905851579002b94bde0ffb
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-12-27 19:44:06 -0800

    HOTFIX: disabled upload from empty gallery

--- client/src/components/HomeGalleryEmpty.vue
+++ client/src/components/HomeGalleryEmpty.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="uppy-select-files flex w-full -mt-2 bg-white rounded border-4 border-dashed border-gray-400 text-gray-500 hover:bg-gray-200 hover:text-gray-600 cursor-pointer transition-all duration-200 ease-in-out"
+    class="Xuppy-select-files flex w-full -mt-2 bg-white rounded border-4 border-dashed border-gray-400 text-gray-500 hover:bg-gray-200 hover:text-gray-600 cursor-pointer transition-all duration-200 ease-in-out"
   >
     <p class="w-full self-center text-center text-2xl">
       <slot>
