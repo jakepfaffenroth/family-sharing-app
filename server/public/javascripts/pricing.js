8bf14eda6eff7a5cde7d3f0a6d336e862392f050
jakepfaffenroth <jakepfaffenroth@gmail.com>
2021-01-14 21:12:04 -0800

    Pricing Table

--- server/public/javascripts/pricing.js
+++ server/public/javascripts/pricing.js
@@ -1,3 +1,28 @@
+// const headerLabels = ['', 'Basic', 'Premium'];
+// const headerCol = [
+//   'Upload limit',
+//   'Image quality',
+//   'Albums',
+//   'Guest downloads',
+//   'Print ordering',
+//   'Private sharing',
+// ];
+// const details = [
+//   ['500', '5000'],
+//   ['Compressed & resized', 'Original'],
+//   ['', 'Unlimited'],
+//   ['', 'coming soom'],
+//   ['coming soon', 'coming soom'],
+//   ['', ''],
+// ];
+
+// const table = document.getElementById('comparison-table');
+// const header = document.createElement('TR');
+// headerLabels.forEach((x) => {
+//   // header.append((document.createElement('TH').innerText = x));
+//   // table.append(header);
+// });
+
 const priceBtns = document.getElementsByClassName('priceBtn');
 for (const btn of priceBtns) {
   btn.addEventListener('click', () => {
